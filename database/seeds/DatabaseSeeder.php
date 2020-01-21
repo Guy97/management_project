3f06b7016a8ce0f3b2b28e10b519f12024756d4c
Guy97 <marcello.pajntar@ied.edu>
2020-01-22 00:07:51 +0100

    aggiunta relazione tra utente e regione

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->delete();
         $this->call(UsersTableSeeder::class);
+
+        DB::table('regions')->delete();
+        $this->call(RegionTableSeeder::class);
     }
 }
